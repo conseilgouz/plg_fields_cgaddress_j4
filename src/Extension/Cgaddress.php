6f8e83c4ccf97bd143cfb03639478e86524e7268
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-10-03 17:05:17 +0200

    1.1.0
    Use ConseilGouz Library
    Display plugin's version
    Use SubscriberInterface

--- src/Extension/Cgaddress.php
+++ src/Extension/Cgaddress.php
@@ -1,7 +1,7 @@
 <?php
 /*
-* CG Address Plugin  - Joomla 4.x/5.x 
-; copyright 		: Copyright (C) 2024 ConseilGouz. All rights reserved.
+* CG Address Plugin  - Joomla 4.x/5.x/6.x
+; copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 ; license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 */
 namespace ConseilGouz\Plugin\Fields\Cgaddress\Extension;
@@ -9,14 +9,32 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Form\Form;
 use Joomla\CMS\Form\FormHelper;
 use Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin;
+use Joomla\Event\SubscriberInterface;
 /**
  * Fields Text Plugin
  *
  */
-class Cgaddress extends FieldsPlugin
+class Cgaddress extends FieldsPlugin implements SubscriberInterface
 {
-	public function onCustomFieldsPrepareDom($field, \DOMElement $parent, Form $form)
+    /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   5.3.0
+     */
+    public static function getSubscribedEvents(): array
     {
+        return array_merge(parent::getSubscribedEvents(), [
+            'onCustomFieldsPrepareDom' => 'prepareDom',
+        ]);
+    }
+    public function prepareDom($event) //($field, \DOMElement $parent, Form $form)
+    {
+        $field = $event[0];
+        $parent = $event[1];
+        $form = $event[2];
+
         $fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
         
         if (!$fieldNode)
